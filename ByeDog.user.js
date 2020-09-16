ab508da12096cb696d5aba2db3ff0fc1b14485f9
djginf <71382476+djginf@users.noreply.github.com>
2020-09-16 13:02:13 +0200

    Add files via upload

---
+++ ByeDog.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @name         ByeDog
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Well one line to destroy HÖK! :)
+// @author       BB
+// @match        https://moodle.uni-corvinus.hu/
+// @grant        none
+// ==/UserScript==
+
+$('.colorbox').remove();
---
+++ README.md
@@ -0,0 +1,10 @@
+# ByeDog
+No more HÖK DOG
+
+Telepítés:
+
+1. Legyen feltelepítve TamperMonkey extension a böngésződbe.
+2. Kattints az alábbi linkre: [Telepítés](https://github.com/djginf/byedog/raw/master/ByeDog.user.js)
+3. Megnyíló ablakon pedig Telepítés gombra.
+
+Kész is vagy nincs több kutya :/
